e7977b7e82eb86dae64953c833bffa4a6599ba91
gaoshu883 <gaoshu883@163.com>
2016-12-20 10:05:02 +0800

    Use superclass and subclass to write code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
-// Enemies our player must avoid
-// @param {number} x - The x coordinate of enemy position
-// @param {number} y - The y coordinate of enemy position
-// @param {number} speed - The speed of enemy moves along the street
-var Enemy = function(x, y, speed) {
+// Entity - superclass
+// @param {number} x - The x coordinate of entity
+// @param {number} y - The y coordinate of entity
+// @param {number} sprite - The image url of entity
+var Entity = function(x, y, sprite) {
     this.x = x;
     this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = sprite;
 };
+
+// Draw entity on the screen
+Entity.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Enemies - subclass - our player must avoid
+// Inherit some properties from entity
+// @param {number} x - The x coordinate of enemy position
+// @param {number} y - The y coordinate of enemy position
+// @param {number} sprite - The image url of enemy
+// @param {number} speed - The speed of enemy moves along the street
+var Enemy = function(x, y, sprite, speed) {
+    Entity.call(this, x, y, sprite);
+    this.speed = speed;
+};
+
+// Inherit render method from entity
+Enemy.prototype = Object.create(Entity.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // @param {number} dt - A time delta between ticks
@@ -25,21 +44,20 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Player in the game
+// Player - subclass
+// Inherit some properties from entity
 // @param {number} x - The x coordinate of player initial position
 // @param {number} y - The y coordinate of player initial position
-var Player = function(x, y) {
-    this.x = x;
-    this.y = y;
+// @param {number} sprite - The image url of player
+var Player = function(x, y, sprite) {
+    Entity.call(this, x, y, sprite);
     this.dx = 0;
     this.dy = 0;
-    this.sprite = 'images/char-boy.png';
 };
+
+// Inherit render method from entity
+Player.prototype = Object.create(Entity.prototype);
+Player.prototype.constructor = Player;
 
 // Update the player's position
 Player.prototype.update = function() {
@@ -56,11 +74,6 @@ Player.prototype.update = function() {
     if (this.y > 356) {
         this.y = 356;
     }
-};
-
-// Draw the player on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Handle with the user input
@@ -88,44 +101,41 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
-// Button to start game
-// @param {number} url - The url of button image
-// @param {number} x - The x coordinate of button image
-// @param {number} y - The y coordinate of button image
+// Button - subclass - interact with canvas
+// Inherit some properties from Entity
+// @param {number} x - The x coordinate of button
+// @param {number} y - The y coordinate of button
+// @param {number} sprite - The url of button image
 // @param {number} width - The width of button image
 // @param {number} height - The height of button image
-function Button(url, x, y, width, height) {
-    this.sprite = url;
-    this.x = x;
-    this.y = y;
+function Button(x, y, sprite, width, height) {
+    Entity.call(this, x, y, sprite);
     this.width = width;
     this.height = height;
     this.activated = false;
 }
 
-// Draw the button on the screen
-Button.prototype.render = function() {
-    ctx.clearRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Inherit render method from Entity
+Button.prototype = Object.create(Entity.prototype);
+Button.prototype.constructor = Button;
 
 // Instantiate enemies, player and buttons objects.
 var allEnemies = [
-    new Enemy(-101, 10, 200),
-    new Enemy(-101, 92, 120),
-    new Enemy(-101, 174, 320),
-    new Enemy(-101, 10, 300),
-    new Enemy(-101, 92, 180),
-    new Enemy(-101, 174, 260),
-    new Enemy(-101, 10, 260),
-    new Enemy(-101, 174, 200)
+    new Enemy(-101, 10, 'images/enemy-bug.png', 200),
+    new Enemy(-101, 92, 'images/enemy-bug.png', 120),
+    new Enemy(-101, 174, 'images/enemy-bug.png', 320),
+    new Enemy(-101, 10, 'images/enemy-bug.png', 300),
+    new Enemy(-101, 92, 'images/enemy-bug.png', 180),
+    new Enemy(-101, 174, 'images/enemy-bug.png', 260),
+    new Enemy(-101, 10, 'images/enemy-bug.png', 260),
+    new Enemy(-101, 174, 'images/enemy-bug.png', 200)
 ];
-var player = new Player(200, 330);
+var player = new Player(200, 330, 'images/char-boy.png');
 
-var startBtn = new Button('images/start-0.png', 178, 200, 149, 46);
-var startBtnActivated = new Button('images/start-1.png', 178, 200, 149, 46);
-var againBtn = new Button('images/again-0.png', 124, 320, 257, 53);
-var againBtnActivated = new Button('images/again-1.png', 124, 320, 257, 53);
+var startBtn = new Button(178, 200, 'images/start-0.png', 149, 46);
+var startBtnActivated = new Button(178, 200, 'images/start-1.png', 149, 46);
+var againBtn = new Button(124, 320, 'images/again-0.png', 257, 53);
+var againBtnActivated = new Button(124, 320, 'images/again-1.png', 257, 53);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method.
